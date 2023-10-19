9f93a52d05d2167cf97b3472ff06349ad9508f0f
Guilherme-de-Melo-Werneck <guilhermemwerneck@hotmail.com>
2023-10-19 09:58:15 -0300

    Faculdade

---
+++ SistemaFinanceiroUGB232/Controllers/ApoioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaFinanceiroUGB232.Controllers
+{
+    public class ApoioController : Controller
+    {
+        public IActionResult Empresa()
+        {
+            return View();
+        }
+    }
+}
---
+++ SistemaFinanceiroUGB232/Views/Apoio/Empresa.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SistemaFinanceiroUGB232.Views.Apoio
+{
+    public class EmpresaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
