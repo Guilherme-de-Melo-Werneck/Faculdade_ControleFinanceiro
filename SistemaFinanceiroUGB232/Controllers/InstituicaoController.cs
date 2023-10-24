d9d66f8a3c321ccc328866601032cab8189f8d8d
Guilherme-de-Melo-Werneck <guilhermemwerneck@hotmail.com>
2023-10-24 20:49:12 -0300

    Faculdade

--- SistemaFinanceiroUGB232/Controllers/InstituicaoController.cs
+++ SistemaFinanceiroUGB232/Controllers/InstituicaoController.cs
@@ -32,15 +32,28 @@ namespace SistemaFinanceiroUGB232.Controllers
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Create(Instituicao instituicao)
+
         {
             instituicao.InstituicaoID = instituicoes.Select(i => i.InstituicaoID).Max() + 1;
             instituicoes.Add(instituicao);
             return RedirectToAction("Index");
         }
+
         public IActionResult Edit(long id)
+        {
+            return View(instituicoes.Where(i => i.InstituicaoID == id).First());
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Instituicao instituicao)
+        {
+            instituicoes.Remove(instituicoes.Where(i => i.InstituicaoID == instituicao.InstituicaoID).First());
+            instituicoes.Add(instituicao);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Details(long id)
         {
             return View(instituicoes.Where(i => i.InstituicaoID == id).First());
         }
     }
 }
-
