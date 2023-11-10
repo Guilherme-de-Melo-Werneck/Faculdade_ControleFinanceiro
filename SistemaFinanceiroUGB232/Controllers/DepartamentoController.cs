068620512afbb68f36d04d593b18c882e1e80f8b
Guilherme-de-Melo-Werneck <guilhermemwerneck@hotmail.com>
2023-11-10 19:54:52 -0300

    Faculdade

--- SistemaFinanceiroUGB232/Controllers/DepartamentoController.cs
+++ SistemaFinanceiroUGB232/Controllers/DepartamentoController.cs
@@ -3,24 +3,65 @@ using SistemaFinanceiroUGB232.Models;
 
 namespace SistemaFinanceiroUGB232.Controllers
 {
-        public class DepartamentoController : Controller
-        {
-            public static IList<Departamento> departamentos = new List<Departamento>()
+    public class DepartamentoController : Controller
+    {
+        private static IList<Departamento> departamentos = new List<Departamento>()
         {
             new Departamento()
             {
                 DepartamentoID = 1,
-                Nome = "Administrativo",
+                Nome = "Hogwarts",
             },
             new Departamento()
             {
                 DepartamentoID = 2,
-                Nome = "Financeiro"
+                Nome = "Mansão X",
             }
         };
-            public IActionResult Index()
+        public IActionResult Index()
         {
             return View(departamentos);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Departamento departamento)
+
+        {
+            departamento.DepartamentoID = departamentos.Select(i => i.DepartamentoID).Max() + 1;
+            departamentos.Add(departamento);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(long id)
+        {
+            return View(departamentos.Where(i => i.DepartamentoID == id).First());
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Departamento departamento)
+        {
+            departamentos.Remove(departamentos.Where(i => i.DepartamentoID == departamento.DepartamentoID).First());
+            departamentos.Add(departamento);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Details(long id)
+        {
+            return View(departamentos.Where(i => i.DepartamentoID == id).First());
+        }
+        public IActionResult Delete(long id)
+        {
+            return View(departamentos.Where(i => i.DepartamentoID == id).First());
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Departamento departamento)
+        {
+            departamentos.Remove(departamentos.Where(i => i.DepartamentoID == departamento.DepartamentoID).First());
+            return RedirectToAction("Index");
         }
     }
 }
--- SistemaFinanceiroUGB232/Models/Departamento.cs
+++ SistemaFinanceiroUGB232/Models/Departamento.cs
@@ -1,7 +1,10 @@
-﻿namespace SistemaFinanceiroUGB232.Models
+﻿using System.ComponentModel;
+
+namespace SistemaFinanceiroUGB232.Models
 {
     public class Departamento
     {
+        [DisplayName("Código")]
         public long? DepartamentoID { get;set; }
         public string Nome  { get; set; }
 
